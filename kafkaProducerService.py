8a9bd2caf98abd5aa9b36e36ce6e09b0721a3c29
rmyers19 <rm2@email.sc.edu>
2018-05-03 14:44:37 -0400

    Not sure where I was. Past tutorial level so saving herer

---
+++ kafkaProducerService.py
@@ -0,0 +1,48 @@
+import threading, logging, time
+import multiprocessing
+
+from kafka import  KafkaProducer
+
+
+def sendStreamM(topicN, tContent):
+    producer = KafkaProducer(bootstrap_servers='172.20.0.2:9092')
+    producer.send(topicN, tContent)
+    producer.close()
+
+class Producer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+        
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers='172.20.0.2:9092')
+
+        #while not self.stop_event.is_set():
+        #    producer.send('test', b"test")
+        #    producer.send('test', b"\xc2Hola, mundo!")
+        #    time.sleep(3)
+
+        producer.close()
+
+
+def main():
+    kafkProducer = Producer()
+
+    kafkProducer.start()
+
+    time.sleep(17)
+    
+    kafkProducer.stop()
+
+    kafkProducer.join()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+        )
+    main()
